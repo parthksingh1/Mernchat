4702430b9e13f79b32016d887984b1fe0cf35a58
Parth Kumar Singh <parthksingh1@gmail.com>
2026-07-12 20:27:20 +0530

    feat(backend): support typing status socket broadcasts

---
+++ server/src/lib/socket.js
@@ -0,0 +1,74 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [
+            "http://localhost:5173",
+            "https://mer-nchat.vercel.app"
+        ],
+        credentials: true,
+    },
+});
+
+export const getReceiverSocketId = (userId) => {
+    return userSocketMap[userId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if (userId) userSocketMap[userId] = socket.id;
+
+    // io.emit() is used to send events to all the connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    // Call Events
+    socket.on("callUser", (data) => {
+        const receiverSocketId = getReceiverSocketId(data.userToCall);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("callUser", {
+                signal: data.signalData,
+                from: data.from,
+                name: data.name
+            });
+        }
+    });
+
+    socket.on("answerCall", (data) => {
+        const receiverSocketId = getReceiverSocketId(data.to);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("callAccepted", data.signal);
+        }
+    });
+
+    // Typing Events
+    socket.on("typing", (data) => {
+        const receiverSocketId = getReceiverSocketId(data.receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("typing", { senderId: userId });
+        }
+    });
+
+    socket.on("stopTyping", (data) => {
+        const receiverSocketId = getReceiverSocketId(data.receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("stopTyping", { senderId: userId });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
+
+export { io, app, server };
